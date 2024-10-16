68f775f4034791220ec58fc360934042da2150e5
GiovanaMarachi <marachigiovana@gmail.com>
2024-10-16 07:14:31 -0300

    feat(folder): cambio de path en los menus para optimizacion de rutas

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,69 +1,36 @@
 import { Redirect, Route } from 'react-router-dom';
-import { IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, school, calendar, business, briefcase, people, idCardOutline } from 'ionicons/icons'; // Iconos más representativos
-import Carrera from '../pages/Carrera';
+import Carrera from '../pages/Facultad';
 import Personas from '../pages/Personas';
-import PeriodoLectivo from '../pages/PeriodoLectivo';
 import Pasantia from '../pages/Pasantia';
 import Proyectos from '../pages/Proyectos';
+import Page from '../pages/Page';
+import { IonRouterOutlet } from '@ionic/react';
 
 
 const Folder: React.FC = () => (
-  <IonTabs>
-    <IonRouterOutlet>
-     
-      <Route exact path="/folder/Personas">
-        <Personas />
-      </Route>
-      <Route exact path="/folder/Carrera">
-        <Carrera />
-      </Route>
-      
-      <Route exact path="/folder/PeriodoLectivo">
-        <PeriodoLectivo />
-      </Route>
-      <Route exact path="/folder/Pasantia">
-        <Pasantia />
-      </Route>
-      <Route exact path="/folder/Proyectos">
-        <Proyectos />
-      </Route>
+  <IonRouterOutlet id="main">
     
-      <Route exact path="/folder">
-        <Redirect to="/folder/Carrera" />
-      </Route>
-      
-    </IonRouterOutlet>
+    <Route exact path="/:facultad/personas" component={Personas}/>
 
-    <IonTabBar slot="bottom">
-      <IonTabButton tab="Carrera" href="/folder/Carrera">
-        <IonIcon icon={school} />
-        <IonLabel>Carrera</IonLabel>
-      </IonTabButton>
+    <Route exact path="/:facultad/facultad" component={Carrera}/>
+    
+    
+    <Route exact path="/:facultad/pasantia">
+      <Pasantia />
+    </Route>
+    <Route exact path="/:facultad/proyectos">
+      <Proyectos />
+    </Route>
+  
+    <Route path='/' exact>
+      <Page/>
+    </Route>
 
-      <IonTabButton tab="Personas" href="/folder/Personas">
-        <IonIcon icon={people} />
-        <IonLabel>Personas</IonLabel>
-      </IonTabButton>
-
-      <IonTabButton tab="PeriodoLectivo" href="/folder/PeriodoLectivo">
-        <IonIcon icon={calendar} />
-        <IonLabel>Periodo Lectivo</IonLabel>
-      </IonTabButton>
-      
-      <IonTabButton tab="Pasantia" href="/folder/Pasantia">
-        <IonIcon icon={briefcase} />
-        <IonLabel>Pasantía</IonLabel>
-      </IonTabButton>
-      
-      <IonTabButton tab="Proyectos" href="/folder/Proyectos">
-        <IonIcon icon={business} />
-        <IonLabel>Proyectos</IonLabel>
-      </IonTabButton>
-      
-    </IonTabBar>
-  </IonTabs>
+    <Route exact path="/folder">
+      <Redirect to="/" />
+    </Route>
+    
+  </IonRouterOutlet>
 );
 
 export default Folder;
